177d243cf697005f87ac8382f69f5a5090062d2b
Eli Vargas <eevargas@gmail.com>
2024-09-10 21:21:58 -0400

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,44 @@
+import ReactDOM from 'react-dom/client'
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Blog from './pages/Blog';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />, // layout
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Blog',
+        element: <Blog />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(
+    <RouterProvider router={router} />
+  );
